777de6b663d8df922995d40f88d2d43259b0c449
dustyshaw <shustydaw@gmail.com>
2025-01-29 11:22:56 -0700

    added web api

---
+++ RaftApi/Program.cs
@@ -0,0 +1,38 @@
+using Raft;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+// My code
+Node n = new([], null);
+
+app.MapPost("/request/SendAppendEntriesRPC", (int term, int prevLogIndex, List<Entry> entries, int commitIndex) =>
+{
+	n.SendAppendEntriesRPC();
+});
+
+app.MapPost("/receive/RecieveAppendEntries", ())
+
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+	public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
